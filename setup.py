cd8bef49a42d814c2ee8f3c66663de0c80c7aec5
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-12-10 11:39:28 +0900

    Change version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psychometrics",
-    version="0.1.1",
+    version="0.1.2",
     description="psychometrics is a library for psychological measurement experiment",
     packages=["psychometrics"],
     install_requires=open("requirements.txt").read().splitlines(),
